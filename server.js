dd426e304dad891408caa3a05aaadffc71e90984
wedesignstudios <deelden@gmail.com>
2017-01-10 06:40:14 -0600

    Done

--- server.js
+++ server.js
@@ -3,12 +3,84 @@
 const http         = require('http');
 const finalhandler = require('finalhandler');
 const Router       = require('router');
+const urlParser    = require('url');
+const querystring  = require('querystring');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
 
-const router = new Router();
+const saltRounds = 10;
+const router = new Router({ mergeParams: true });
 
-router.get('/', (request, response) => {
-  // A good place to start!
-  response.end();
+router.use(bodyParser.json());
+
+let messages = [];
+let id = 1;
+
+
+class Message {
+  constructor(message) {
+    this.id = id;
+    this.message = message.message;
+    id++;
+  }
+}
+
+router.get('/', (request, response) => {  
+  response.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  response.end('Hello, World!');
+});
+
+router.post('/message', (request, response) => {
+  let newMes;
+  
+  response.setHeader('Content-Type', 'application/json; charset=utf-8');
+  if(!request.body) {
+    response.statusCode = 400;
+  }
+
+  newMes = new Message(request.body);
+  messages.push(newMes); 
+  response.end(JSON.stringify(newMes.id));
+});
+
+
+router.get('/messages', (request, response) => {
+  let url = urlParser.parse(request.url)
+  let queryStr = querystring.parse(url.query);
+
+  const result = JSON.stringify(messages)
+
+  response.setHeader('Content-Type', 'application/json; charset=utf-8');
+
+  if(queryStr.encrypt === 'true') {
+    response.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    console.log(queryStr.encrypt);
+    return bcrypt.hash(result, saltRounds, (err, hash) => {
+      response.end(hash);
+    });
+  }
+
+  response.end(result);  
+});
+
+
+router.get('/message/:id', (request, response) => {
+  let paramId = request.params.id;
+  let message = messages[paramId-1];
+  let queryStr = request._parsedUrl.query;
+
+  const result = JSON.stringify(message);
+
+  response.setHeader('Content-Type', 'application/json; charset=utf-8');
+
+  if(queryStr === 'encrypt=true') {
+    response.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    return bcrypt.hash(result, saltRounds, (err, hash) => {
+      response.end(hash);
+    });
+  }
+
+  response.end(result);
 });
 
 const server = http.createServer((request, response) => {
