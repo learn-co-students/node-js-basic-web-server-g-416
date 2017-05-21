a0f1e1f6831a52496e6ca6a11da91d17a730b3a0
Colton Staab <colton.staab@gmail.com>
2017-05-21 14:55:49 +0000

    Required some new librarie i server.js

--- server.js
+++ server.js
@@ -3,12 +3,20 @@
 const http         = require('http');
 const finalhandler = require('finalhandler');
 const Router       = require('router');
+const bodyParser = require('body-parser');
 
 const router = new Router();
+router.use(bodyParser.json());
+
+let messages = [];
 
 router.get('/', (request, response) => {
   response.setHeader('Content-Type', 'text/plain; charset=utf-8');
   response.end('Hello, World!');
+});
+
+route.post('/message',(request,response)=>{
+
 });
 
 const server = http.createServer((request, response) => {
