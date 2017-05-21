9116bf32bd3119f617669d80d650b0c5e8792de5
Colton Staab <colton.staab@gmail.com>
2017-05-21 15:04:56 +0000

    Made some changes

--- server.js
+++ server.js
@@ -1,22 +1,71 @@
 "use strict";
 
-const http         = require('http');
 const finalhandler = require('finalhandler');
+const bodyParser   = require('body-parser');
+const http         = require('http');
+const urlParser    = require('url');
+const querystring  = require('querystring');
 const Router       = require('router');
-const bodyParser = require('body-parser');
+const bcrypt       = require('bcrypt');
 
-const router = new Router();
-router.use(bodyParser.json());
+const router = new Router({ mergerParams: true });
 
 let messages = [];
+let nextId = 1;
+
+class Message {
+  constructor(message) {
+    this.id = nextId;
+    this.message = message;
+    nextId++;
+  }
+}
+
+// Use body parser to handle post data.
+router.use(bodyParser.json());
 
 router.get('/', (request, response) => {
   response.setHeader('Content-Type', 'text/plain; charset=utf-8');
   response.end('Hello, World!');
 });
 
-route.post('/message',(request,response)=>{
+router.get('/messages', (request, response) => {
+  let url = urlParser.parse(request.url),
+      params = querystring.parse(url.query);
 
+  let result = JSON.stringify(messages);
+
+  response.setHeader('Content-Type', 'application/json; charset=utf-8');
+
+  if (params.encrypt) {
+    response.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    return bcrypt.hash(result, 10, (error, hashed) => {
+      if (error) {
+        throw new Error();
+      }
+      response.end(hashed);
+    });
+  }
+
+  response.end(result);
+});
+
+router.post('/message', (request, response) => {
+  let newMsg;
+
+  response.setHeader('Content-Type', 'application/json; charset=utf-8');
+
+  if (!request.body.message) {
+    response.statusCode = 400;
+    response.statusMessage = 'No message provided.';
+    response.end();
+    return;
+  }
+
+  newMsg = new Message(request.body.message);
+  messages.push(newMsg);
+
+  response.end(JSON.stringify(newMsg.id));
 });
 
 const server = http.createServer((request, response) => {
